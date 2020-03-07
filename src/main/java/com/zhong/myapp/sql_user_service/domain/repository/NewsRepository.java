f91bf51c124951659c66d8051145f12c5d5c0c92
zhonggunm@hotmail.com <zhonggunm@hotmail.com>
2020-03-07 12:33:33 -0700

    delete placeholder files

--- src/main/java/com/zhong/myapp/sql_user_service/domain/repository/NewsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zhong.myapp.sql_user_service.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zhong.myapp.sql_user_service.domain.models.News;
-
-public interface NewsRepository extends JpaRepository<News, String> {
-
-}
--- src/main/java/com/zhong/myapp/sql_user_service/domain/service/NewsService.java
+++
@@ -1,8 +0,0 @@
-package com.zhong.myapp.sql_user_service.domain.service;
-
-/**
- * @author claudioed on 29/10/17. Project cms
- */
-public class NewsService {
-
-}
